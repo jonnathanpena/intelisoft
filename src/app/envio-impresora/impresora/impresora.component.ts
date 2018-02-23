e6488ddeaa00af1ad38b1c43d457e1602c12c6e7
Josybett Campos <josycamgon@hotmail.com>
2018-02-23 17:46:56 -0500

    Primera version de lookandfeel

--- src/app/envio-impresora/impresora/impresora.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-impresora',
-  templateUrl: './impresora.component.html',
-  styleUrls: ['./impresora.component.scss']
-})
-export class ImpresoraComponent {}
